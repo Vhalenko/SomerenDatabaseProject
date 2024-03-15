1be471c528ef5e70cb8d5ffd8edb076fb3f0bfa3
OrestPokotylenko <opokotylenkowork@gmail.com>
2024-03-15 22:33:35 +0100

    Order class added

---
+++ SomerenModel/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SomerenModel
+{
+    internal class Order
+    {
+        private int _Quantity;
+        public Student Student { get; private set; }
+        public Drink Drink { get; private set; }
+        public int Quantity { get => _Quantity; private set => _Quantity = (value > 0) ? value : throw new Exception("Select 1 or more"); }
+
+        public Order (Student student, Drink drink, int quantity)
+        {
+            Student = student;
+            Drink = drink;
+            Quantity = quantity;
+        }   
+    }
+}

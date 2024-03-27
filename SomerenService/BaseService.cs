72379c8f2a404538c601599b2b2bd9071ff29349
OrestPokotylenko <opokotylenkowork@gmail.com>
2024-03-27 12:04:46 +0100

    BaseService added

---
+++ SomerenService/BaseService.cs
@@ -0,0 +1,29 @@
+﻿using SomerenDAL;
+using SomerenModel;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace SomerenService
+{
+    public abstract class BaseService<T>
+    {
+        protected const int ObjectIdBeforeDb = 0;
+
+        /*Add item*/
+
+        public void AddDrink(List<string> list)
+        {
+            if (list.Any(x => x == ""))
+            {
+                throw new Exception("Enter all values!");
+            }
+            else
+            {
+                FillItemToAdd(list);
+            }
+        }
+
+        protected abstract void FillItemToAdd(List<string> list);
+    }
+}
